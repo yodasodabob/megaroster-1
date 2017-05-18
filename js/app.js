ca3d50cdbb8e4d6bb74014fd7ae89f6fb149900c
Laurence Ruberl <bknd225@gmail.com>
2017-05-17 21:27:45 -0400

    Things and stuff; there was a bug with code so lots of changes here

--- js/app.js
+++ js/app.js
@@ -15,10 +15,30 @@ const megaroster = {
       .addEventListener('submit', this.addStudent.bind(this))
   },
 
+  moveElementInArray (array, value, item, positionChange) {
+    let oldIndex = value;
+    console.log(oldIndex)
+    if (oldIndex > -1){
+      let newIndex = (oldIndex + positionChange);
+
+      if (newIndex < 0){
+        newIndex = 0
+      }else if (newIndex >= array.length){
+        newIndex = array.length
+      }
+      console.log(newIndex)
+      let arrayClone = array.slice();
+      arrayClone.splice(oldIndex,1);
+      arrayClone.splice(newIndex,0,item);
+
+      return arrayClone
+    }
+    return array
+  },
+
   removeStudent(ev) {
     const btn = ev.target
     for (let i = 0; i < this.students.length; i++) {
-      console.log(this.students[i].id, btn.closest('.student').dataset.id)
       if (this.students[i].id == btn.closest('.student').dataset.id) {
         this.students.splice(i, 1)
         break
@@ -27,10 +47,34 @@ const megaroster = {
     btn.closest('.student').remove()
   },
 
-promotestudent(ev) {
-  const btn = ev.target
-  btn.closest('.student').style.backgroundColor = #00ff00
-},
+  promoteStudent(ev) {
+    const btn = ev.target
+    btn.closest('.student').style.backgroundColor = '#00ff00'
+  },
+
+  moveUp(ev) {
+    const btn = ev.target
+    for (let i = 0; i < this.students.length; i++) {
+      if (this.students[i].id == btn.closest('.student').dataset.id) {
+        this.students = this.moveElementInArray(this.students, i, this.students[i], -1)
+        break
+      }
+    }
+    console.log(this)
+    this.studentList.insertBefore(btn.closest('.student'), btn.closest('.student').previousSibling)
+  },
+
+  moveDown(ev) {
+    const btn = ev.target
+    for (let i = 0; i < this.students.length; i++) {
+      if (this.students[i].id == btn.closest('.student').dataset.id) {
+        this.students = this.moveElementInArray(this.students, i, this.students[i], 1)
+        break
+      }
+    }
+    console.log(this)
+    this.studentList.insertBefore(btn.closest('.student'), btn.closest('.student').nextSibling.nextSibling)
+  },
 
   addStudent(ev) {
     ev.preventDefault()
@@ -59,6 +103,8 @@ promotestudent(ev) {
     li.dataset.id = student.id
     li.querySelector('button.remove').addEventListener('click', this.removeStudent.bind(this))
     li.querySelector('button.promote').addEventListener('click', this.promoteStudent.bind(this))
+    li.querySelector('button.up').addEventListener('click', this.moveUp.bind(this))
+    li.querySelector('button.down').addEventListener('click', this.moveDown.bind(this))
     return li
   },
 
